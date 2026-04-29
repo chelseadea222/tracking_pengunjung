710dfa8b4fbba4b6c77fe119ff746a48352f7e4a
chelseadea222 <chelseadea222@gmail.com>
2026-04-29 22:52:08 +0700

    baru

--- api/config.php
+++ api/config.php
@@ -12,7 +12,7 @@ if (!file_exists($ca_path)) {
 
 try {
     $pdo = new PDO(
-        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", 
+        "mysql:host=gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com;port=4000;dbname=Tracking;charset=utf8mb4", 
         $username, 
         $password,
         [
