36f8c1edcea42933bdb5ac26ec2c5298ea8df0c4
chelseadea222 <chelseadea222@gmail.com>
2026-04-29 21:44:58 +0700

    up

--- api/proses_login.php
+++ api/proses_login.php
@@ -10,18 +10,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (!$email || !$password) {
         $error = 'Email dan password wajib diisi!';
     } else {
-        $stmt = $pdo->prepare("SELECT * FROM users WHERE email = :email"); 
-        $stmt->execute([ ':email' => $email ]); 
-        $user = $stmt->fetch(); 
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+    $stmt->execute([
+    ':email' => $email
+    ]);
 
-        // Pengecekan password
-        if ($user && password_verify($password, $user['password'])) { 
-            
-            // Set Session seperti biasa
-            $_SESSION['user_id'] = $user['id']; 
-            $_SESSION['nama'] = $user['nama']; 
-            $_SESSION['email'] = $user['email']; 
-            $_SESSION['role'] = $user['role']; 
+        $user = $stmt->fetch();
+
+        if ($user && password_verify($password, $user['password'])) {
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['nama'] = $user['nama'];
+            $_SESSION['email'] = $user['email'];
+            $_SESSION['role'] = $user['role'];
 
             // Pastikan session disimpan sebelum redirect
             session_write_close();
