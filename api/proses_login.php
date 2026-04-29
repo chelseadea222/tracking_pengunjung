6a9ff5132dc1a60b2530fd3bd5f28287fbbcc512
chelseadea222 <chelseadea222@gmail.com>
2026-04-29 15:27:18 +0700

    baru

--- api/proses_login.php
+++ api/proses_login.php
@@ -10,23 +10,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (!$email || !$password) {
         $error = 'Email dan password wajib diisi!';
     } else {
-        $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
-        $stmt->execute([$email]);
+        $stmt = $pdo->prepare("SELECT * FROM users WHERE email = $email AND password = $password");
+        $stmt->execute([$email, $password]);
         $user = $stmt->fetch();
 
         // Pengecekan password
-        if ($user && password_verify($password, $user['password'])) {
-            $_SESSION['user_id'] = $user['id'];
-            $_SESSION['nama'] = $user['nama'];
-            $_SESSION['email'] = $user['email'];
-            $_SESSION['role'] = $user['role'];
+        if ($users && password_verify($password, $users['password'])) {
+            $_SESSION['user_id'] = $users['id'];
+            $_SESSION['nama'] = $users['nama'];
+            $_SESSION['email'] = $users['email'];
+            $_SESSION['role'] = $users['role'];
 
             // Pastikan session disimpan sebelum redirect
             session_write_close();
 
-            $target_url = (strtolower($user['role']) === 'admin') ? 'tiket_harian.php' : 'tiket.php';
-            header("Location: " . $target_url, true, 302);
-            exit;
+            if (isset($_SESSION['role'])) {
+            if (strtolower($_SESSION['role']) === 'admin') {
+                header('Location: tiket_harian.php');
+                exit;
+            } else {
+                header('Location: tiket.php');
+                exit;
+        }
+        }
             
         } else {
             $error = 'Email atau password salah!';
