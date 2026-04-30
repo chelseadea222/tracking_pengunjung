a3b787ea5bc0e56340e3e9b714f97ac24662a865
chelseadea222 <chelseadea222@gmail.com>
2026-04-30 12:02:20 +0700

    var dump session

--- api/tiket_harian.php
+++ api/tiket_harian.php
@@ -4,14 +4,15 @@ if (session_status() === PHP_SESSION_NONE) {
     session_save_path('/tmp');
     session_start();
 }
-
-if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
-    header("Location: /login.php");
-    exit();
-}
-
 var_dump($_SESSION);
 die();
+
+// if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
+//     header("Location: /login.php");
+//     exit();
+// }
+
+
 
 require_once __DIR__ . '/api_tiket.php';
 require_once __DIR__ . '/proses_tiket_harian.php';
